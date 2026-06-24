5a1c47360b86cffb9c8a4caeda80a7475ae5c06a
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2026-06-24 14:51:08 -0300

    empty project

---
+++ ms_config/src/main/kotlin/com/example/ms_config/MsConfigApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ms_config
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsConfigApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsConfigApplication>(*args)
+}
---
+++ ms_order/src/main/kotlin/com/example/ms_order/MsOrderApplication.kt
@@ -0,0 +1,11 @@
+package com.example.ms_order
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsOrderApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsOrderApplication>(*args)
+}
