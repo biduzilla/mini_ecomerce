0cad758e5da274daa3ae17fffb78cd10e6b45fc8
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2026-06-24 15:04:55 -0300

    feat: config server implemented

--- ms_config/src/main/kotlin/com/example/ms_config/MsConfigApplication.kt
+++ ms_config/src/main/kotlin/com/example/ms_config/MsConfigApplication.kt
@@ -1,9 +1,12 @@
 package com.example.ms_config
 
+import jdk.jfr.Enabled
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
 
 @SpringBootApplication
+@EnableConfigServer
 class MsConfigApplication
 
 fun main(args: Array<String>) {
